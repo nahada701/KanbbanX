b350c94dee6656066685b6dd134e45dd59271968
Nahada C <nahadac701@gmail.com>
2024-12-17 13:35:23 +0530

    server changed

--- src/Services/serverURL.js
+++ src/Services/serverURL.js
@@ -1,1 +1,3 @@
-export const server_url="https://kanban-server-bf4o.onrender.com"
+export const server_url="https://kanban-server-bf4o.onrender.com "
+// http://localhost:3000
+// https://kanban-server-bf4o.onrender.com
